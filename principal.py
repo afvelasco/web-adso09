3c3bf8f3fb522314c2d789126725477228446871
Andres F Velasco <afvelasco@gmail.com>
2025-10-27 09:04:57 -0500

    Se inicia construcción de login

--- principal.py
+++ principal.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 programa = Flask(__name__)
 
 @programa.route("/")
 def index():
-    return "Esta es mi primer web-sita"
+    return render_template("index.html")
+
+@programa.route("/login", methods=['POST'])
+def login():
+    usua = request.form["usua"]
+    contra = request.form["contra"]
+    if usua=="afvelasco" and contra=="1234":
+        return render_template("bienvenido.html")
+    else:
+        return render_template("index.html",msg = "Credenciales incorrectas")
+    
 
 if __name__=="__main__":
     programa.run(host="0.0.0.0",port="5000",debug=True)
