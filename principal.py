6f77e1dce5254056882312db1b9a289ac65ce9fc
Andres F Velasco <afvelasco@gmail.com>
2025-11-24 07:50:18 -0500

    Se incluyen ver y agregar productos

--- principal.py
+++ principal.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, redirect, render_template, request, send_from_directory
 import mysql.connector
 import hashlib
 
@@ -9,6 +10,11 @@ mi_db = mysql.connector.connect(host="localhost",
                                 password="",
                                 database="db-adso09")
 mi_cursor = mi_db.cursor()
+programa.config['CARPETA_UP'] = os.path.join('uploads')
+
+@programa.route("/uploads/<nombre>")
+def uploads(nombre):
+    return send_from_directory(programa.config['CARPETA_UP'],nombre)
 
 @programa.route("/")
 def index():
@@ -23,12 +29,43 @@ def login():
     resultado = mi_cursor.fetchall()
     cifrada = hashlib.sha512(contra.encode("utf-8")).hexdigest()
     if len(resultado)>0 and cifrada==resultado[0][3]:
-        sql = "SELECT * FROM productos"
-        mi_cursor.execute(sql)
-        resultado = mi_cursor.fetchall()
-        return render_template("productos.html", productos = resultado)
+        return redirect("/productos")
     else:
         return render_template("index.html",msg = "Credenciales incorrectas")
-    
+
+@programa.route("/productos")
+def productos():
+    sql = "SELECT * FROM productos WHERE inactivo=0"
+    mi_cursor.execute(sql)
+    resultado = mi_cursor.fetchall()
+    return render_template("productos.html", productos = resultado)
+
+@programa.route("/agregaproducto")
+def agregaproducto():
+    return render_template("agregaproducto.html")
+
+@programa.route("/guardaproducto", methods=['POST'])
+def guardaproducto():
+    id = request.form['id']
+    nom = request.form['nom']
+    pre = request.form['pre']
+    sal = request.form['sal']
+    foto = request.files['foto']
+    nom,ext =os.path.splitext(foto.filename)
+    foto_nueva = 'P'+id+ext
+    foto.save("uploads/"+foto_nueva)
+    sql = f"INSERT INTO productos (idproducto,nombre,precio,saldo,foto) VALUES ('{id}','{nom}',{pre},{sal},'{foto_nueva}')"
+    mi_cursor.execute(sql)
+    mi_db.commit()
+    return redirect('/productos')
+
+@programa.route("/borraproducto/<id>")
+def borraproducto(id):
+    sql = f"UPDATE productos SET inactivo=1 WHERE idproducto='{id}'"
+    print(id)
+    mi_cursor.execute(sql)
+    mi_db.commit()
+    return redirect("/productos")
+
 if __name__=="__main__":
     programa.run(host="0.0.0.0",port="5000",debug=True)
